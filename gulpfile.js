288928365511c870e1e363e4c1270f16877ea2a7
levlele <leandrovilanova@gmail.com>
2017-02-06 18:02:10 -0300

    Test Mpboxes

--- gulpfile.js
+++ gulpfile.js
@@ -38,12 +38,12 @@ var build = {
     img: "build/img/**/*",
     html: "build/index.html",
     json_internas: "build/json/merchants_internas.json", // Información de los merchants en categorias y slider
-    // json_promo: "build/test.json",
-    // json_promo: "build/17_01_20_home_deco.json",
-    // json_promo: "build/17_01_23_exclusiva_grisino.json",
-    // json_promo: "build/17_01_24_compartida_hotday_mshops.json",
+    json_promo: "build/json/test.json"
+    // json_promo: "build/json/17_01_20_home_deco.json",
+    // json_promo: "build/json/17_01_23_exclusiva_grisino.json",
+    // json_promo: "build/json/17_01_24_compartida_hotday_mshops.json",
     // json_promo: "build/json/17_01_27_compartida_bensimon_pepas.json",
-    json_promo: "build/json/17_02_01_exclusiva_topper.json"
+    // json_promo: "build/json/17_02_01_exclusiva_topper.json"
 };
 
 var dist = {
